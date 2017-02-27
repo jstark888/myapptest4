9e6c67562385d1a9695569d4005b12e7f902c80c
Jeff Stark <jstark888@gmail.com>
2017-02-27 12:59:51 -1000

    Added field someField and getter adn setter

--- myapptest4dao/src/main/java/jstark/test/TestDao.java
+++ myapptest4dao/src/main/java/jstark/test/TestDao.java
@@ -4,4 +4,13 @@ package jstark.test;
  * Created by jstar on 2/26/2017.
  */
 public class TestDao {
+    private String someField = null;
+
+    public String getSomeField() {
+        return someField;
+    }
+
+    public void setSomeField(String someField) {
+        this.someField = someField;
+    }
 }
